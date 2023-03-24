63354fc5f6d41239b03f8917e1d45e2a487a5693
Igor <Igo@wifi-student11-436.sri.ucl.ac.be>
2023-03-24 14:15:38 +0100

    add all question for tutor

--- analyse_de_trace.py
+++ analyse_de_trace.py
@@ -1,11 +1,9 @@
 import pyshark
 import time
-capture = pyshark.FileCapture("Paquet WireShark screen share paquet 148.pcapng",display_filter="")
+capture = pyshark.FileCapture("visiocall_discord_wifi_eduroam.pcapng",display_filter="tcp")
 j = 1
 for i in capture:
     i.pretty_print()
-    if j == 34:
-         print(i.tcp.field_names)
-         time.sleep(20)
+    time.sleep(20)
     print("packet n° " + str(j))
     j += 1
